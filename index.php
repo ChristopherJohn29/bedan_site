90ba146d4f52aea46693b9ebe385a8bd5599ede2
christophergamo29 <christopherjohngamo@gmail.com>
2025-02-18 02:35:28 +0800

    update

--- index.php
+++ index.php
@@ -214,7 +214,7 @@ $(document).ready(function() {
                 if (selectedStation == 2) {
                     if(destinationStation == 1){
                         console.log('testing');
-                        totalTravelTimeInMinutes = travelTimeBetweenStations['2'] + travelTimeBetweenStations['3'] + travelTimeBetweenStations['2'] + travelTimeBetweenStations['1'] * 60;
+                        totalTravelTimeInMinutes = (travelTimeBetweenStations['2'] + travelTimeBetweenStations['3'] + travelTimeBetweenStations['2'] + travelTimeBetweenStations['1']) * 60;
                         console.log(totalTravelTimeInMinutes);
                     } else {
                         totalTravelTimeInMinutes = travelTimeBetweenStations['1'] * 60;
