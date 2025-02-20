1de4f0241c09ad61ed138958bc2d5c48229b08c1
christophergamo29 <christopherjohngamo@gmail.com>
2025-02-20 15:49:08 +0800

    update

--- run_python.php
+++ run_python.php
@@ -5,6 +5,8 @@ $pattern = "python " . $scriptPath;
 // Use double quotes to ensure the pattern is treated as a single argument.
 exec("pgrep -f \"$pattern\"", $output);
 
+var_dump($output);
+
 if (empty($output)) {
     exec("sudo nohup python $scriptPath > /dev/null 2>&1 &");
     echo "Python script started.";
