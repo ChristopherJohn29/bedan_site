fde281e68a3c3681c787af49fa5cb3752c852ab0
christophergamo29 <christopherjohngamo@gmail.com>
2025-02-20 23:19:37 +0800

    update

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
   />
   <link 
     rel="stylesheet" 
-    href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css"
+    href="css/bootstrap-icons.css"
   />
   
   <style>
