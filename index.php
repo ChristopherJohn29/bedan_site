4d87602de9a2342f9e0a22ac818039c9e553c915
christophergamo29 <christopherjohngamo@gmail.com>
2025-02-20 15:21:04 +0800

    update

--- index.php
+++ index.php
@@ -5,12 +5,10 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Train Tracker</title>
   
-  <!-- Bootstrap CSS -->
   <link
     href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css"
     rel="stylesheet"
   />
-  <!-- Bootstrap Icons (Optional) -->
   <link 
     rel="stylesheet" 
     href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css"
@@ -391,12 +389,7 @@ $(document).ready(function() {
            }
        }
 
-       // (Optional) Add waiting time at each station if needed
-       // totalTravelTimeInSeconds += waitingTimeAtStation * numberOfStops;
-
-       // Subtract elapsed time
        var remainingTimeInSeconds = totalTravelTimeInSeconds - timeDifferenceInSeconds;
-
 
        var secondsLeft = Math.floor(remainingTimeInSeconds % 60);
        
